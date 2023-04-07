4f1f2e51d3c633b56ee8a6daaf76ed1336ae7c74
eeva.alanko <eeva.alanko@siili.com>
2023-04-07 19:17:32 +0300

    Exercise 12_8

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -8,13 +8,13 @@ router.get('/', async (_, res) => {
   res.send(todos);
 });
 
-/* GET todo by id. */
+/* GET by id. */
 router.get('/:id', async (req, res) => {
   const todo = await Todo.find({_id: req.params.id})
   res.send(todo);
 });
 
-/* update todo by id. */
+/* update by id. */
 router.put('/:id', async (req, res) => {
   const todo = await Todo.updateOne(
     { "_id": req.params.id },
